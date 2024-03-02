aa0575780592a07ff2ebd327f5acf6d3064a4db1
Joshua <joshuatanaka@gmail.com>
2024-03-01 22:49:57 -0800

    Title Screen and Minor Additions
    Fixed movement, added additional moving mechanic (Possible
    puzzle/gameplay potential), fixed positioning variables, starting title
    screen

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,8 +2,13 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -15,35 +20,69 @@ public class GamePanel extends JPanel implements Runnable {
 	Snake snake;
 	Food food;
 	Interactions interactions;
-	JLabel label = new JLabel();
-	JPanel panel = new JPanel();
+	JLabel scoreLabel = new JLabel();
 	KeyHandler key = new KeyHandler();
+	JButton button;
+	JPanel titleScreen = new JPanel();
+	JPanel greyTint = new JPanel();
+	Font font = new Font("Arial", Font.BOLD, 15);
 	public final int size = 42;
 	public final int rows = 16;
 	public final int cols = 16;
 	public final int screenSize = size * rows;
+	private boolean gameStatus = true;
 
 	public GamePanel() {
+		// Snake Game Panel Sizes
 		this.setPreferredSize(new Dimension(screenSize, screenSize));
 		this.setBackground(Color.black);
+		this.setLayout(null);
 		this.setFocusable(true);
 		this.addKeyListener(key);
-		this.add(panel);
-		panel.add(label);
+
+		// Starting Title Screen to start game as well
+		titleScreen.setBackground(Color.GRAY);
+		titleScreen.setBounds((screenSize / 2) - (size * 4) / 2, (screenSize / 2) - (size * 3), size * 4, size * 2);
+		// 10 --> 5, 4
+		button = new JButton("Start Game");
+		button.setBackground(Color.GRAY);
+		button.setBounds((screenSize / 2) - (size * 4) / 2, (screenSize / 2) - (size * 1), size * 4, size * 1);
+		button.setFont(font);
+
+		button.addActionListener(e -> {
+			gameStatus = true;
+			titleScreen.setVisible(false);
+			greyTint.setVisible(false);
+			// Starts Game
+			thread = new Thread(this);
+			thread.start();
+		});
+
+		titleScreen.add(button);
+		titleScreen.setVisible(true);
+		this.add(titleScreen);
+
+		// Score
+		scoreLabel.setBounds((screenSize / 2) - (size * 2) / 2, 0, size * 2, size * 1);
+		scoreLabel.setFont(font);
+		this.add(scoreLabel);
+
+		// Entities Creation
 		snake = new Snake(this, key, null, null);
 		food = new Food(this, snake);
 		interactions = new Interactions(this, snake, food);
+
 		updateScore();
+
+		greyTint.setBackground(new Color(128, 128, 128, 50));
+		greyTint.setBounds(0, 0, screenSize, screenSize);
+		greyTint.setVisible(true);
+		this.add(greyTint);	
 	}
 
-	public void startGame() {
-		thread = new Thread(this);
-		thread.start();
-	}
-	
 	@Override
 	public void run() {
-		while (thread != null) {
+		while (thread != null && gameStatus == true) {
 			try {
 				Thread.sleep(500);
 				update();
@@ -54,32 +93,31 @@ public class GamePanel extends JPanel implements Runnable {
 		}
 	}
 
-	public void updateScore() {
-		label.setText("Score: " + interactions.getScore());
-	}
-
 	public void update() {
 		snake.update();
 		interactions.update();
-		Snake pointer = snake.next(); // check the first snake with interactions then the while loop 
-		while(pointer != null) {
+		Snake pointer = snake.next(); // check the first snake with interactions then the while loop
+		while (pointer != null) {
 			pointer.update();
 			pointer = pointer.next();
 		}
+
 		food.update();
 		updateScore();
-//		System.out.println(food.getX() + " " + food.getY() + " " + snake.getX() + ", " + snake.getY());
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Snake pointer = snake;
-		while(pointer !=null) {
+		while (pointer != null) {
 			pointer.draw(g);
 			pointer = pointer.next();
 		}
 		food.draw(g);
 	}
-	
-}
 
+	public void updateScore() {
+		scoreLabel.setText("Score: " + interactions.getScore());
+	}
+
+}
